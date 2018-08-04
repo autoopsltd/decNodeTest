46381ec879ec75c6420284d6d2033bb435d9110c
autoopsltd <autoopsltd@outlook.com>
2018-08-04 21:14:35 +0100

    Fixed

--- main.js
+++ main.js
@@ -7,6 +7,6 @@ var server = http.createServer(function (request, response) {
   response.end("Hello Auto Ops Ltd.\n");
 });
 
-// listen on localhost:8000
-server.listen(8000);
-console.log("Server listening at http://127.0.0.1:8000/");
+// listen on localhost:3000
+server.listen(3000);
+console.log("Server listening at http://127.0.0.1:3000/");
