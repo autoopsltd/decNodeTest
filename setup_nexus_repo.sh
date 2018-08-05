32b28ab66194e86008d5f09c8cdc2426ab204164
autoopsltd <autoopsltd@outlook.com>
2018-08-05 18:19:44 +0100

    Added Nexus

--- setup_nexus_repo.sh
+++ setup_nexus_repo.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-echo "registry=http://192.168.1.15:8082/repository/npm-internal/" > .npmrc
+echo "email=jenkins@jenkins.com" > .npmrc
+echo "always-auth=true" >> .npmrc
 echo "_auth=YWRtaW46YWRtaW4xMjM=" >> .npmrc
-
+echo "registry=http://192.168.1.15:8082/repository/npm-internal/" >> .npmrc
